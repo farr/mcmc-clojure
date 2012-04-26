1c94c9077fdc2d7e553b1a17b54b2cb3acf1787e
Will M. Farr <w-farr@northwestern.edu>
2012-04-26 10:37:27 -0500

    Moved to clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mcmc "1.0.0-SNAPSHOT"
   :description "Markov Chain Monte Carlo library and related tools."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.commons/commons-math "2.2"]]
   :java-source-path "src"
   :warn-on-reflection true)
